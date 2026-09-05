f1f0216a2f1c59a7ebd3464483624d70ff6c17b5
Alex Kalesnikevich <cracher@Alexs-MacBook-Pro.local>
2026-09-05 13:10:40 +0300

    pre-migration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,42 +1,9 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
 
-const dirname =
-	typeof __dirname !== 'undefined'
-		? __dirname
-		: path.dirname(fileURLToPath(import.meta.url))
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
 	test: {
-		projects: [
-			{
-				test: {
-					name: 'storybook',
-					browser: {
-						enabled: true,
-						headless: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }],
-					},
-					setupFiles: ['.storybook/vitest.setup.ts'],
-				},
-			},
-			{
-				test: {
-					name: 'booking',
-					include: ['src/features/booking/tests/**/*.{test,spec}.{js,ts}'],
-					environment: 'node',
-					testTimeout: 10000,
-				},
-			},
-		],
+		include: ['src/features/booking/tests/**/*.{test,spec}.{js,ts}'],
+		environment: 'node',
+		testTimeout: 10000,
 	},
 })
